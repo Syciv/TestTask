0f00e77e84820864460f2e8175b7dcbf824a71b9
Syciv <sycivan22@gmail.com>
2021-12-16 22:42:44 +0500

    sql в liquibase

---
+++ target/classes/db/changelog/db.changelog-master.sql
@@ -0,0 +1,39 @@
+--liquibase formatted sql
+
+--changeset liquibase:1
+
+CREATE TABLE filials(
+    id serial NOT NULL PRIMARY KEY,
+    name varchar
+);
+
+CREATE TABLE posts(
+    id serial NOT NULL PRIMARY KEY,
+    name varchar
+);
+
+CREATE TABLE employees(
+    id serial NOT NULL PRIMARY KEY,
+    postid integer,
+    name varchar,
+    filialid integer,
+    chiefId integer,
+    FOREIGN KEY (chiefId) REFERENCES employees(id),
+    FOREIGN KEY (filialid) REFERENCES filials(id),
+    FOREIGN KEY (postid) REFERENCES posts(id)
+);
+
+CREATE TABLE Tasks(
+    id serial NOT NULL PRIMARY KEY,
+    priority integer,
+    description text,
+    employeeId integer,
+    FOREIGN KEY (employeeId) REFERENCES employees(id)
+);
+
+--changeset liquibase:2
+
+INSERT INTO filials(name) values ('Северный'), ('Южный'), ('Западный'), ('Восточный');
+INSERT INTO posts(name) values ('Инженер'), ('Механик'), ('Медик'), ('Капитан');
+
+
